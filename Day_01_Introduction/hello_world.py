8c5936de9570af654e93d96b9ef0c660d1dcce9f
shubhammate013 <mateshubham7478@gmail.com>
2024-02-04 23:10:42 +0530

    successfully added

---
+++ Day_01_Introduction/hello_world.py
@@ -0,0 +1,20 @@
+print("hello_world")
+
+# print a string using varible
+s = "hello"
+print(s)
+
+#calculation of two simple number using variable
+a = 13
+b = 11
+c = a + b
+print(c)
+
+#sum of two numbrs using function
+def calculate(a, b):
+    result = a + b
+    return result
+
+# Example usage:
+result = calculate(13, 55)
+print(result)
