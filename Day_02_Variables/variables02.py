1fe6b024662748017a366ab48e324a1405e53aca
shubhammate013 <mateshubham7478@gmail.com>
2024-02-05 22:32:46 +0530

    this is also done

---
+++ Day_02_Variables/variables02.py
@@ -0,0 +1,43 @@
+# Static Vs Dynamic Typing
+# Static Vs Dynamic Binding
+# stylish declaration techniques
+
+name = 'Shubham'
+print(name)
+
+a = 5
+b = 6
+print(a + b)
+
+
+# Dynamic Typing in python
+a = 5
+# Static Typing in other language
+# int a = 5 
+
+# Dynamic Binding
+a = 5
+print(a)
+a = 'shubham'
+print(a)
+
+# Static Binding
+# int a = 5
+
+# special or stylish syntax example
+a = 5;b=6;c=7
+print(a)
+print(b)
+print(c)
+
+a = 1
+b = 2
+c = 3
+print(a,b,c)
+
+
+a,b,c = 1,2,3
+print(a,b,c)
+
+a=b=c= 5
+print(a,b,c)
