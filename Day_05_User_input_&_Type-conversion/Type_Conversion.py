59806edec12f922b38aae78a59fbb7c7071f299f
shubhammate013 <mateshubham7478@gmail.com>
2024-02-08 14:20:26 +0530

    successfully added

---
+++ Day_05_User_input_&_Type-conversion/Type_Conversion.py
@@ -0,0 +1,24 @@
+# In Python, you can convert data from one type to another using type conversion functions. Some commonly used type conversion functions include:
+
+  # 1. int(): Converts a value to an integer.
+  # 2. float(): Converts a value to a floating-point number.
+  # 3. str(): Converts a value to a string.
+  # 4. bool(): Converts a value to a Boolean.
+
+# Convert to integer
+num_string = "10"
+num_integer = int(num_string)
+print(num_integer)  # Output: 10
+
+# Convert to float
+num_float = float(num_string)
+print(num_float)  # Output: 10.0
+
+# Convert to string
+num_str = str(num_float)
+print(num_str)  # Output: '10.0'
+
+# Convert to boolean
+bool_value = bool(num_integer)
+print(bool_value)  # Output: True if num_integer is not zero, False otherwise
+
---
+++ Day_05_User_input_&_Type-conversion/User_input.py
@@ -0,0 +1,15 @@
+name = input('Chal Apna Naam Bata')
+
+first_num = input("Enter the first number")
+second_num = input("Enter the second number")
+print(first_num)
+print(second_num)
+
+# Before the execution of this code output will be different.
+result = first_num+second_num
+print(result)
+
+# After the execution of this code output will be different
+result = int(first_num) + int(second_num)
+print(result)
+
